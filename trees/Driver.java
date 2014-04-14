2017ac0d9ecc86a2c86709a1a03a8b539aea5c09
Matthew Bourque <matthewkbourque@gmail.com>
2014-04-13 22:01:27 -0400

    Added methods to compare two trees, delete nodes from a tree, amongst other small upgrades.

--- trees/Driver.java
+++ trees/Driver.java
@@ -9,8 +9,6 @@ Algorithm:
 Purpose:
 */
 
-import java.util.ArrayList;
-
 class Driver {
 
   
@@ -22,13 +20,102 @@ class Driver {
         int[] sequence1 = new int[] {60, 20, 100, 35, 15, 200, 75, 150, 6, 17, 40};
         int[] sequence2 = new int[] {60, 100, 20, 15, 35, 75, 200, 6, 17, 40, 150};
     
-        Tree t1 = new Tree(sequence1);
-
-        printInorder(t1);
-        printPostorder(t1);
+        Tree t1 = new Tree();
+        build(t1, sequence1);
+        //printInorder(t1);
+        int numLeafNodes = count(t1.root);
+        //System.out.println(numLeafNodes);
+        
+        Tree t2 = new Tree();
+        build(t2, sequence1);
+        swap(t2.root);
+        //printPostorder(t2);
+        
+        boolean same = compare(t1.root, t2.root);
+        System.out.println(same);
+        
+        Tree t3 = new Tree();
+        build(t3, sequence2);
+        System.out.println(compare(t1.root, t3.root));
+        //printInorder(t3);
+        delete(t3.root, 15);
+        //printInorder(t3);
     }
     
     
+    public static void build(Tree t, int[] sequence) {
+        /*
+         * Builds a Binary Search Tree from given sequence of integers
+        */
+    
+        for (int i=0; i<sequence.length; i++) {
+            int number = sequence[i];
+            TreeNode newNode = new TreeNode(number);
+            if (t.root == null) {t.root = newNode;}
+            t.insert(t.root, newNode);
+        }
+    }
+    
+    
+    public static int count(TreeNode t) {
+        /*
+         * Counts the number of leaf nodes in the tree
+        */
+    
+        if (t == null) {
+            return 0;
+        } else if (t.left == null && t.right == null) {
+            return 1; 
+        } else {
+            return count(t.left) + count(t.right);
+        } 
+    }
+    
+    
+    public static boolean compare(TreeNode a, TreeNode b) {
+        /*
+         * Returns true if tree a is indentical to tree b
+         * Returns false if tree a and tree b are different
+        */
+    
+        if (a == b) {return true;}
+        if (a == null || b == null) {return false;}
+        if (!compare(a.left,b.left)) {return false;}
+        if (!compare(a.right,b.right)) {return false;}
+        
+        return true;
+    }
+        
+       
+    public static TreeNode delete(TreeNode t, int value) {
+        /*
+         * Deletes any instance of 'value' from the tree
+        */
+
+        if (value < (int) t.data) {t.left = delete(t.left, value);}
+        else if (value > (int) t.data) {t.right = delete(t.right, value);}
+        else if (t.left != null && t.right != null) {
+            t.data = minData(t.right);
+            t.right = delete(t.right, (int) t.data);
+        } else {
+            if (t.left != null) {t = t.left;}
+            else {t = t.right;}
+        }
+        
+        return t;
+    }  
+        
+       
+    private static int minData(TreeNode t) {
+        /*
+         * Finds the smallest number in the tree
+        */
+    
+        if (t.left == null) {return (int) t.data;}
+        else {return minData(t.left);}
+    }
+        
+        
     public static void printInorder(Tree t) {
         /*
          * Prints the sequence using inorder method
@@ -51,4 +138,21 @@ class Driver {
         t.postorder(t.root);
         System.out.println("");
     }
+    
+    
+    public static void swap(TreeNode t) {
+        /*
+         * Exchanges the left and right children recursively
+         * for every node in the tree
+        */
+
+        if (t != null) {
+            TreeNode tmp;
+            tmp = t.left;
+            t.left = t.right;
+            t.right = tmp;
+            swap(t.left);
+            swap(t.right);
+        }
+    }  
 }
--- trees/Tree.java
+++ trees/Tree.java
@@ -1,36 +1,10 @@
-import java.util.ArrayList;
-
 public class Tree {
   
     public TreeNode root = null;
-    
-    
-    public Tree(int[] sequence) {
-        /*
-         * The constructor method
-        */
-        
-        build(sequence);
-    }
-    
-    public void build(int[] sequence) {
-        /*
-         * Builds a Binary Search Tree from given sequence of integers
-        */
-    
-        for (int i=0; i<sequence.length; i++) {
-            int number = sequence[i];
-            TreeNode newNode = new TreeNode(number);
-            if (root == null) {root = newNode;}
-            insert(root, newNode);
-        }
-    }
+    public int counter = 0;
     
    
     public void insert(TreeNode t, TreeNode nodeToAdd) {
-        /*
-         * Determines where in the BST to add the new node and adds it
-        */
 
         if ((int) nodeToAdd.data < (int) t.data) {
             if (t.left == null) {t.left = nodeToAdd;}
@@ -40,24 +14,6 @@ public class Tree {
             if (t.right == null) {t.right = nodeToAdd;}
             else {insert(t.right, nodeToAdd);}      
         }
-    }
-    
-    
-    public TreeNode delete(TreeNode t, int k) {
-    
-        if (t == null) {return null;}
-        if (k < (int) t.data) {t.left = delete(t.left, k);}
-        else if (k > (int) t.data) {t.right = delete(t.right, k);}
-        else {
-            if (t.right == null) {return t.left;}
-            if (t.left == null) {return t.right;}
-            TreeNode tmp = t;
-            t.data = minData(tmp.right);
-            t.right = deleteMin(t.right);
-            t.left = tmp.left;
-        }
-    
-        return t;
     }
    
   
@@ -95,20 +51,5 @@ public class Tree {
             postorder(t.right);
             System.out.print(t.data + " ");
         }
-    }
-    
-    
-    private int minData(TreeNode t) {
-    
-        if (t.left == null) {return (int) t.data;}
-        else {return minData(t.left);}
-    }
-    
-    
-    private TreeNode deleteMin(TreeNode t) {
-      
-        if (t.left == null) {return t.right;}
-        t.left = deleteMin(t.left);
-        return t;
     }
 }
