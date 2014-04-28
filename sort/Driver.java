04fb7059a83360fa7be279144934275381c59e0a
Matthew Bourque <matthewkbourque@gmail.com>
2014-04-28 14:56:00 -0400

    Added algorithm and purpose to header.

--- sort/Driver.java
+++ sort/Driver.java
@@ -5,13 +5,49 @@ Optional Project 2
 
 Algorithm:
 
-Purpose:
+Generate file of random integers
+Build linked list from random integers
+Build integer array from random integers
 
+Grab current clock time
+Perform insertion sort on linked list:
+Grab current clock time
+Compute and display total time it took for insertion sort
+Write sorted linked list to a file
+
+Grab current clock time
+Perform quick sort on linked list:
+Grab current clock time
+Compute and display total time it took for quick sort
+Write sorted linked list to a file
+
+Grab current clock time
+Perform merge sort on linked list:
+Grab current clock time
+Compute and display total time it took for merge sort
+Write sorted linked list to a file
+
+Grab current clock time
+Perform heap sort on linked list:
+Grab current clock time
+Compute and display total time it took for heap sort
+Write sorted array to a file
+
+Purpose:
+    This program serves to quantify the time complexity of four
+    different sorting algorithms (1) Insertion sort, (2) Quick sort,
+    (3) Merge sort, and (4) Heap sort.  An array of random integers
+    are read in from a file and sorted using the four different
+    techniques.  The execution time of each technique is clocked and
+    printed to the screen for easy comparison between the four.
 */
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Driver {
@@ -23,18 +59,21 @@ public class Driver {
         */
       
         // Generate file of random numbers
-        makeNumberFile();
+        //makeNumberFile();
         
         // Initialize nodes
         Node q = new Node(0, null);
         Node head = q;
         
-        // Make linked list containing the random numbers
-        LinkedList list = new LinkedList();
-        //Node insertNode = list.insert(q, head, "random_numbers.txt");
-        //Node quickNode = list.insert(q, head, "random_numbers.txt");
-        Node mergeNode = list.insert(q, head, "random_numbers.txt");
-        //Node heapNode = list.insert(q, head, "random_numbers.txt");
+        // Make data structures containing the random numbers
+        //LinkedList insertList = new LinkedList();
+        //LinkedList quickList = new LinkedList();
+        //LinkedList mergeList = new LinkedList();
+
+        //Node insertNode = insertList.insert(q, head, "random_numbers.txt");
+        //Node quickNode = quickList.insert(q, head, "random_numbers.txt");
+        //Node mergeNode = mergeList.insert(q, head, "random_numbers.txt");
+        int[] heapArray = getHeapArray("random_numbers.txt");
         
         // Perform insertion sort and write result to "insertion.dat"
         //InsertionSort inserstionSort = new InsertionSort();
@@ -42,7 +81,7 @@ public class Driver {
         //Node sortedInsertion = inserstionSort.sort(insertNode.next);
         //final long insertionSortEnd = System.currentTimeMillis();
         //final long insertionSortTotal = insertionSortEnd - insertionSortBegin;
-        //System.out.println("Insertion sort took: " + insertionSortTotal);
+        //System.out.println("Insertion sort took: " + insertionSortTotal + " ms");
         //writeToFile(sortedInsertion, "insertion.dat");
         
         // Perform quick sort and write result to "quick.dat"
@@ -51,32 +90,32 @@ public class Driver {
         //Node sortedQuick = quickSort.sort(quickNode.next);
         //final long quickSortEnd = System.currentTimeMillis();
         //final long quickSortTotal = quickSortEnd - quickSortBegin;
-        //System.out.println("Quick sort took: " + quickSortTotal);
+        //System.out.println("Quick sort took: " + quickSortTotal + " ms");
         //writeToFile(sortedQuick, "quick.dat");
         
         // Perform merge sort and write result to "merge.dat"
-        MergeSort mergeSort = new MergeSort();
-        final long mergeSortBegin = System.currentTimeMillis();
-        Node sortedMerge = mergeSort.sort(mergeNode.next);       
-        final long mergeSortEnd = System.currentTimeMillis();
-        final long mergeSortTotal = mergeSortEnd - mergeSortBegin;
-        System.out.println("Merge sort took: " + mergeSortTotal);
-        writeToFile(sortedMerge, "merge.dat");
+        //MergeSort mergeSort = new MergeSort();
+        //final long mergeSortBegin = System.currentTimeMillis();
+        //Node sortedMerge = mergeSort.sort(mergeNode.next);       
+        //final long mergeSortEnd = System.currentTimeMillis();
+        //final long mergeSortTotal = mergeSortEnd - mergeSortBegin;
+        //System.out.println("Merge sort took: " + mergeSortTotal + " ms");
+        //writeNodeToFile(sortedMerge, "merge.dat");
         
         // Perform heap sort and write result to "heap.dat"
-        //HeapSort heapSort = new HeapSort();
-        //final long heapSortBegin = System.currentTimeMillis();
-        //Node sortedHeap = heapSort.sort(heapNode.next);       
-        //final long heapSortEnd = System.currentTimeMillis();
-        //final long heapSortTotal = heapSortEnd - heapSortBegin;
-        //System.out.println("Heap sort took: " + heapSortTotal);
-        //writeToFile(sortedHeap, "heap.dat");
+        HeapSort heapSort = new HeapSort();
+        final long heapSortBegin = System.currentTimeMillis();
+        int[] sortedHeap = heapSort.sort(heapArray);       
+        final long heapSortEnd = System.currentTimeMillis();
+        final long heapSortTotal = heapSortEnd - heapSortBegin;
+        System.out.println("Heap sort took: " + heapSortTotal + " ms");
+        writeArrayToFile(sortedHeap, "heap.dat");
     }
     
     
     public static void makeNumberFile() {
         /*
-         * Creates a file containing 10,000 random numbers
+         * Creates a file containing random numbers
         */
     
         int count = 0;
@@ -84,11 +123,10 @@ public class Driver {
         String outputFile = "random_numbers.txt";
     
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-            
-            while (count <= 100) {
-                int randomNumber = random.nextInt((100 - 0) + 1) + 0;
-                writer.write(Integer.toString(randomNumber) + "\n");
+            PrintWriter writer = new PrintWriter(outputFile, "UTF-8");
+            while (count <= 5000) {
+                int randomNumber = random.nextInt();
+                writer.println(Integer.toString(randomNumber));
                 count ++;
             }
         writer.close();
@@ -99,16 +137,33 @@ public class Driver {
     }
     
     
-    public static void writeToFile(Node q, String outputFile) {
+    public static void writeArrayToFile(int[] array, String outputFile) {
         /*
-         * Writes the sorted list to the output file
+         * Writes the sorted array to the output file
         */
       
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+            PrintWriter writer = new PrintWriter(outputFile, "UTF-8");
+            for (int i = array.length - 1; i >= 0; i--) {
+                writer.println(array[i]);
+            }
+        writer.close();
+        System.out.println("File written to " + outputFile);
+        } catch(Exception e) {
+            System.out.println("Error " + e.toString());
+        }            
+    }
+    
+    
+    public static void writeNodeToFile(Node q, String outputFile) {
+        /*
+         * Writes the sorted node to the output file
+        */
+      
+        try {
+            PrintWriter writer = new PrintWriter(outputFile, "UTF-8");
             while (q != null) {
-                System.out.println(q.data);
-                writer.write(q.data);
+                writer.println(q.data);
                 q = q.next;
             }
         writer.close();
@@ -116,5 +171,25 @@ public class Driver {
         } catch(Exception e) {
             System.out.println("Error " + e.toString());
         }            
+    }
+    
+    
+    public static int[] getHeapArray(String inputFile) {
+        /*
+         * Constructs an integer array with the random numbers
+         * read in from the input file
+        */
+      
+        int[] heapArray = new int[5000];
+     
+        try {
+            Scanner input = new Scanner(new File(inputFile));
+            for (int i=0; i<heapArray.length; i++) {
+                heapArray[i] = input.nextInt(); 
+            }
+        } catch (Exception e) {
+            System.out.println("Error " + e.toString());
+        }
+        return heapArray;
     }
 }
--- sort/HeapSort.java
+++ sort/HeapSort.java
@@ -1,10 +1,46 @@
 public class HeapSort {
+  
+    public int[] data;
 
-    public void sort(Node q) {
+    
+    public int[] sort(int[] heapArray) {
       
-      
-      
+        this.data = heapArray;
+        for (int i = heapArray.length / 2 - 1; i>=0; i--) {
+            heapify(i);
+        }
+        
+        for(int i = this.data.length - 1; i>=0; i--) {
+        
+            int temp = this.data[0];
+            this.data[0] = this.data[i];
+            this.data[i] = temp;
+            heapify(i);
+            System.out.println(this.data[0]);
+        }
+        
+        return this.data;
     }
-  
-  
+    
+    
+    public void heapify(int n) {
+    
+        int index = n;
+        int left = 2 * n + 1;
+        int right = 2 * n + 2;
+        
+        if (left < this.data.length && this.data[left] > this.data[index]) {
+            index = left;
+        }
+        if (right < this.data.length && this.data[right] > this.data[index]) {
+            index = right;
+        }
+        
+        if (index != n) {
+            int temp = this.data[n];
+            this.data[n] = this.data[index];
+            this.data[index] = temp;
+            heapify(index);
+        }
+    }
 }
