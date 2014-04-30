00f259fff42c0a1ab77267cd34fb5f229499e485
Matthew Bourque <matthewkbourque@gmail.com>
2014-04-29 20:52:08 -0400

    Initial commit of United States traversal using graphs project.

---
+++ state_traverse/StateWriter.java
@@ -0,0 +1,157 @@
+public class StateWriter {
+
+  
+    public void writeStateName(int stateNum) {
+        /*
+         * Prints the state name based on the stateNum
+        */
+      
+        switch (stateNum) {
+            
+            case 1: 
+                System.out.println("Washington");
+                break;
+            case 2: 
+                System.out.println("Oregon");
+                break;
+            case 3: 
+                System.out.println("California");
+                break;
+            case 4: 
+                System.out.println("Nevada");
+                break;
+            case 5: 
+                System.out.println("Idaho");
+                break;
+            case 6: 
+                System.out.println("Montana");
+                break;
+            case 7: 
+                System.out.println("Utah");
+                break;
+            case 8: 
+                System.out.println("Arizona");
+                break;
+            case 9: 
+                System.out.println("Wyoming");
+                break;
+            case 10: 
+                System.out.println("Colorado");
+                break;
+            case 11: 
+                System.out.println("New Mexico");
+                break;
+            case 12: 
+                System.out.println("Texas");
+                break;
+            case 13: 
+                System.out.println("Oklahoma");
+                break;
+            case 14: 
+                System.out.println("Jansas");
+                break;
+            case 15: 
+                System.out.println("Nebraska");
+                break;
+            case 16: 
+                System.out.println("South Dakota");
+                break;
+            case 17: 
+                System.out.println("North Dakota");
+                break;
+            case 18: 
+                System.out.println("Minnesota");
+                break;
+            case 19: 
+                System.out.println("Iowa");
+                break;
+            case 20: 
+                System.out.println("Missouri");
+                break;
+            case 21: 
+                System.out.println("Arkansas");
+                break;
+            case 22: 
+                System.out.println("Louisiana");
+                break;
+            case 23: 
+                System.out.println("Mississippi");
+                break;
+            case 24: 
+                System.out.println("Tennessee");
+                break;
+            case 25: 
+                System.out.println("Kentucky");
+                break;
+            case 26: 
+                System.out.println("Illinois");
+                break;
+            case 27: 
+                System.out.println("Wisconsin");
+                break;
+            case 28: 
+                System.out.println("Michigan");
+                break;
+            case 29: 
+                System.out.println("Indiana");
+                break;
+            case 30: 
+                System.out.println("Ohio");
+                break;
+            case 31: 
+                System.out.println("Pennsylvania");
+                break;
+            case 32: 
+                System.out.println("New York");
+                break;
+            case 33: 
+                System.out.println("Vermont");
+                break;
+            case 34: 
+                System.out.println("New Hampshire");
+                break;
+            case 35: 
+                System.out.println("Maine");
+                break;
+            case 36: 
+                System.out.println("Massachusetts");
+                break;
+             case 37: 
+                System.out.println("Rhode Island");
+                break;
+            case 38: 
+                System.out.println("Connecticut");
+                break;
+            case 39: 
+                System.out.println("New Jersey");
+                break;
+            case 40: 
+                System.out.println("Delaware");
+                break;
+            case 41: 
+                System.out.println("Maryland");
+                break;
+            case 42: 
+                System.out.println("West Virginia");
+                break;
+            case 43: 
+                System.out.println("Virginia");
+                break;
+            case 44: 
+                System.out.println("North Carolina");
+                break;
+            case 45: 
+                System.out.println("South Carolina");
+                break;
+            case 46: 
+                System.out.println("Georgia");
+                break;
+            case 47: 
+                System.out.println("Alabama");
+                break;
+            case 48: 
+                System.out.println("Florida");
+                break;
+        }
+    }
+}
