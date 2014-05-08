d424e25f1b1be58e9efdc8581a608c9dc5e918fe
Matthew Bourque <matthewkbourque@gmail.com>
2014-05-08 13:21:35 -0400

    Added method for BFS traversal.

--- state_traverse/Driver.java
+++ state_traverse/Driver.java
@@ -1,11 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+
 public class Driver {
 
     public static void main(String[] args) {
         /*
          * The main method
         */
+      
+        Node[] mapForDFS = getMap();
+        Node[] mapForBFS = getMap();
+      
+        Graph graphForDFS = new Graph(mapForDFS);
+        System.out.println("Traversing map using Depth First Traversal:\n");
+        graphForDFS.DFS(mapForDFS[0]);
         
-        Graph graph = new Graph();
-        graph.traverse(); 
+        Graph graphForBFS = new Graph(mapForBFS);
+        System.out.println("\nTraversing map using Breadth First Traversal:\n");
+        graphForBFS.BFS(mapForBFS[0]);
+    }
+    
+    
+    public static Node[] getMap() {
+        /*
+         * Builds map by reading in input file using adjacency list
+         * representation
+        */
+      
+        // Initialize Adjecency List
+        Node[] map = new Node[48];
+        
+        try {
+            // Read in data line by line
+            BufferedReader br = new BufferedReader(new FileReader("map_data.dat"));
+            String line;
+            for (int i=0; i<map.length; i++) {
+                line = br.readLine();
+                
+                // Split each line into integer tokens
+                StringTokenizer st = new StringTokenizer(line);
+                
+                // Initialze new node with index
+                Node q = new Node(i+1, false, null);
+                Node head = q;
+                
+                // Point node to remaining tokens
+                while (st.hasMoreTokens()) {
+                    q.next = new Node(Integer.parseInt(st.nextToken()), false, null);
+                    q = q.next;
+                }
+                
+                // Add linked list to adjecency list
+                map[i] = head;
+            }
+            br.close();
+            
+        } catch (Exception e) {
+            System.out.println("Error " + e.toString());
+        }
+        
+        return map;
     }
 }
--- state_traverse/Graph.java
+++ state_traverse/Graph.java
@@ -1,65 +1,16 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.StringTokenizer;
-
 public class Graph {
-
+  
     public Node[] map;
-
-    public void traverse() {
-      
-        this.map = getMap();
-        
-        System.out.println("Traversing using DFS:\n");
-        DFS(this.map[0]); 
-        
-        System.out.println("\nTraversing using BFS:\n");
-        //BFS(this.map[0]);
-    }
-
-    
-    public Node[] getMap() {
+  
+    public Graph(Node[] map) {
         /*
-         * Builds map by reading in input file using adjacency list
-         * representation
+         * The constructor method
         */
       
-        // Initialize Adjecency List
-        this.map = new Node[48];
-        
-        try {
-            // Read in data line by line
-            BufferedReader br = new BufferedReader(new FileReader("map_data.dat"));
-            String line;
-            for (int i=0; i<this.map.length; i++) {
-                line = br.readLine();
-                
-                // Split each line into integer tokens
-                StringTokenizer st = new StringTokenizer(line);
-                
-                // Initialze new node with index
-                Node q = new Node(i+1, false, null);
-                Node head = q;
-                
-                // Point node to remaining tokens
-                while (st.hasMoreTokens()) {
-                    q.next = new Node(Integer.parseInt(st.nextToken()), false, null);
-                    q = q.next;
-                }
-                
-                // Add linked list to adjecency list
-                this.map[i] = head;
-            }
-            br.close();
-            
-        } catch (Exception e) {
-            System.out.println("Error " + e.toString());
-        }
-        
-        return this.map;
+        this.map = map;
     }
     
- 
+
     public void DFS(Node p) {
         /*
          * Traverses the graph via Depth First Traversal
@@ -84,32 +35,39 @@ public class Graph {
         for (int i=0; i<count; i++) {
             if (p.next != null) {
                 int adj = p.next.data;
-                //System.out.println(adj);
                 Node q = this.map[adj - 1];
                 if (q.visited == false) {DFS(q);}
                 p = p.next;
             }
         }
     }
-}
     
-//    public void DFS(Node s) {
-//        
-//        s.visited = true;
-//        
-//        Stack stack = new Stack();
-//        stack.push(s);
-//        
-//        while (!stack.isEmpty()) {
-//            Node v = stack.pop();
-//            if (v.visited == false) {
-//                v.visited = true;
-//                StateWriter sw = new StateWriter();
-//                sw.writeStateName(v.data);
-//                
-//                Node r = this.map[v.data - 1];
-//                stack.push(r);
-//                r = r.next; 
-//            }
-//        }
-//    }
+    
+    public void BFS(Node p) {
+        /*
+         * Traverses the graph via Breadth First Traversal
+        */
+        
+        Queue queue = new Queue();
+        queue.enq(p.data);
+        
+        StateWriter sw = new StateWriter();
+        sw.writeStateName(p.data);
+        p.visited = true;
+        
+        while (!queue.isEmpty()) {
+            int j = (Integer) queue.dnq();
+            Node n = this.map[j-1];
+            while (n.next != null) {
+                int adj = n.next.data;
+                Node m = this.map[adj -1];
+                if (m.visited == false) {
+                    queue.enq(m.data);
+                    sw.writeStateName(m.data);
+                    m.visited = true;
+                }
+                n = n.next;
+            }
+        }      
+    }
+}
